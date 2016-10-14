1735a86f9dc955c1334eeccf56a264cc2763578f
swxy <swxymile@163.com>
2016-10-14 14:02:11 +0800

    添加删除

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,7 +12,14 @@ export const addTodo = (text, date)=> (dispatch, getState) => {
         return dispatch({type: types.ADD_TODO, text, date});
     });
 };
-export const deleteTodo = id => ({type: types.DELETE_TODO, id});
+export const deleteTodo = id => (dispatch) => {
+    deleteTodoFromDb(id).then(res => {
+        return dispatch({type: types.DELETE_TODO, id});
+    }, err => {
+        console.error(err);
+        getTodos();
+    });
+};
 export const editTodo = (id, text, date) => ({type: types.EDIT_TODO, id, text, date});
 export const completeTodo = id => ({type: types.COMPLETE_TODO, id});
 export const completeAll = () => ({type: types.COMPLETE_ALL});
