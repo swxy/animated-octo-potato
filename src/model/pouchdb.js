81517c68dd050bf0e943cf9798e70ba656fe501d
swxy <swxymile@163.com>
2016-10-15 17:27:44 +0800

    add pouchdb

---
+++ src/model/pouchdb.js
@@ -0,0 +1,47 @@
+import PouchDB from 'pouchdb-browser';
+
+const db = new PouchDB('PouchDB_TODO');
+
+
+export const addTodoToDb = (todo) => {
+    const id = +new Date();
+    return db.put({
+        _id: todo.id + '',
+        ...todo
+    }).then( res => {
+        console.log('add success');
+        return res;
+    })
+};
+
+export const deleteTodoFromDb = todo => {
+    return db.remove(todo).then(res => {
+        console.log('delete success');
+        return res;
+    });
+};
+
+export const updateTodoSyncDb = (todo) => {
+    return db.put(todo);
+};
+
+export const getTodosFromDb = (doFilter) => {
+    return db.allDocs({include_docs: true, attachments: true});
+};
+
+export const getRawTodosFromDb = () => {
+    return db.todo;
+};
+
+
+export const openDb = () => {
+    return db.open();
+};
+
+export const closeDb = () => {
+    console.log('db closed');
+    return db.close();
+};
+
+export default db;
+
